231a6728e93b0638d56866c3784d1e7692b6e674
FireDmitry <coyetskya@gmail.com>
2022-10-19 21:23:37 +0300

    Lab 2

---
+++ Lab 2/Lab 2/Lab 2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>;
+using namespace std;
+
+int main()
+{
+	int x, y, z;
+	cout << "input x:";
+	cin >> x;
+	cout << "input y:";
+	cin >> y;
+	z = x + y;
+	cout << x << "+" << y << "=" << z << endl;
+	return 0;
+
+
+
+
+}
