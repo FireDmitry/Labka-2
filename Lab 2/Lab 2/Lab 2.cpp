c998b5488c779c2dfccd5c0f9f59a94dff6534c1
FireDmitry <114346686+FireDmitry@users.noreply.github.com>
2022-11-10 00:09:09 +0200

    Update Lab 2.cpp

--- Lab 2/Lab 2/Lab 2.cpp
+++ Lab 2/Lab 2/Lab 2.cpp
@@ -1,11 +1,11 @@
-#include<iostream>;
+#include<iostream>;//Бібліотека
 using namespace std;
 
 int main()
 {
-	int x, y, z;
-	cout << "input x:";
-	cin >> x;
+	int x, y, z;//Оголошення зміних типу int(цілі)
+	cout << "input x:"; /*Виведення x на консоль*/
+	cin >> x;/*Виведення в x на консоль*/
 	cout << "input y:";
 	cin >> y;
 	z = x + y;
